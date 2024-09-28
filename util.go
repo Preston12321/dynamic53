91f17c71aac761d7d2cd5405c85b04d0f99a51e5
Preston Locke <locke_preston@yahoo.com>
2024-09-28 17:13:20 -0500

    Fix missing commas between AllowEditingRecords statements in generated IAM policies

--- util.go
+++ util.go
@@ -52,8 +52,20 @@ func GetPublicIPv4(ctx context.Context) (net.IP, error) {
 	return ipv4, nil
 }
 
+// listSeparator provides a clean way to write templates that involve lists
+// needing separators that can't be repeated after the final item, e.g. JSON
+// lists. Given the length of the list, and the current index, listSeparator
+// will either return the given separator or an empty string.
+func listSeparator(length int, index int, separator string) string {
+	if index == length-1 {
+		return ""
+	}
+	return separator
+}
+
 func GenerateIAMPolicy(cfg DaemonConfig) (string, error) {
-	tmpl, err := template.New("iam-policy").Parse(IAM_POLICY_TEMPLATE)
+	funcs := template.FuncMap{"listSeparator": listSeparator}
+	tmpl, err := template.New("iam-policy").Funcs(funcs).Parse(IAM_POLICY_TEMPLATE)
 	if err != nil {
 		return "", fmt.Errorf("unable to parse policy template: %w", err)
 	}
